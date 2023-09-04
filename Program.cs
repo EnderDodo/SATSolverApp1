f48d1f53948559dff2733d95ff614fcdf1b078d4
Denis Urbanskiy <Urbanskiy565@gmail.com>
2023-09-04 17:02:48 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using SatSolverLib;
+
+switch (args.Length)
+{
+    case 0:
+        throw new ArgumentException("File path is not found");
+    case > 1:
+        throw new ArgumentException("There are more than one file paths");
+}
+
+var filePath = args[0].Replace("\"", "");
+
+if (!File.Exists(filePath))
+    throw new ArgumentException($"File on path {filePath} does not exist");
+
+var cnf = DimacsParser.ParseFile(filePath);
+var isSat = Solver.Dpll(cnf, out var solution);
+
+DimacsParser.WriteModelToConsole(solution);
